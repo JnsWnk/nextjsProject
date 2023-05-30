a89d134b818b9aefa26401eb913a3a1e124aa030
JnsWnk <wnkjns@gmail.com>
2023-05-30 23:39:15 +0200

    Bugfix for Vercel.

--- src/lib/credential.tsx
+++ src/lib/credential.tsx
@@ -1,17 +1,13 @@
 import { db } from "./db";
 import bcrypt from "bcryptjs";
 
-const salt = bcrypt.genSaltSync(10);
-
-export async function signUp() {}
-
 export async function login(email: string, password: string) {
   const user = await db.user.findFirst({
     where: {
       email: email,
     },
   });
-  if (user) {
+  if (user && user.password) {
     const match = await bcrypt.compare(password, user.password);
     if (match) {
       return user;
